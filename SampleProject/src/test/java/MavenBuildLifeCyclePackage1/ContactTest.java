3660ad55516038a28e5bea0488bddc00ba921f2f
admin <admin@DESKTOP-3VI02IA>
2025-06-26 16:43:09 +0530

    First Push

---
+++ SampleProject/src/test/java/MavenBuildLifeCyclePackage1/ContactTest.java
@@ -0,0 +1,18 @@
+package MavenBuildLifeCyclePackage1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("Execute createContactTest");
+	}
+	
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("Execute modifyContactTest");
+	}
+}
---
+++ SampleProject/src/test/java/MavenBuildLifeCyclePackage2/OrgTest.java
@@ -0,0 +1,18 @@
+package MavenBuildLifeCyclePackage2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Execute createOrgTest");
+	}
+	
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("Execute modifyOrgTest");
+	}
+}
